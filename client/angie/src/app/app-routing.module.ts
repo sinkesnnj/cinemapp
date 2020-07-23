eedf2ac16fe359730dd727b5941a177b4195de82
Siniša Ilibašić <sinkesnnj@gmail.com>
2020-07-23 17:42:16 +0200

    Added skeleton of the app

--- client/angie/src/app/app-routing.module.ts
+++ client/angie/src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { MoviesComponent } from './modules/movies/movies.component';
+import { TheatresComponent } from './modules/theatres/theatres.component';
+import { NewsComponent } from './modules/news/news.component';
+import { SessionComponent } from './modules/session/session.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'movies', component: MoviesComponent },
+  { path: 'theatres', component: TheatresComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'sign_in', component: SessionComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/angie/src/app/app.module.ts
+++ client/angie/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { NavigationComponent } from './shared/navigation/navigation.component';
+import { MoviesComponent } from './modules/movies/movies.component';
+import { TheatresComponent } from './modules/theatres/theatres.component';
+import { NewsComponent } from './modules/news/news.component';
+import { SessionComponent } from './modules/session/session.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavigationComponent,
+    MoviesComponent,
+    TheatresComponent,
+    NewsComponent,
+    SessionComponent
   ],
   imports: [
     BrowserModule,
