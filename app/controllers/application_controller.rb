0795902a5f23a3c0a17410033ad0e0564696183d
Siniša Ilibašić <sinkesnnj@gmail.com>
2020-09-04 10:36:50 +0200

    Reveret

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
-  skip_before_action :verify_authenticity_token
 
   def is_admin?
     return current_user.present? && Role.find_by_sql(["SELECT name FROM roles r JOIN user_roles ur ON r.id = ur.role_id WHERE r.name = :role AND ur.user_id = :user_id", {role: 'Admin', user_id: current_user.id}]).present?
